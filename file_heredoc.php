bcc4333454dee510d4352f0f9333eeffe0252393
NURKHOLIS <kholis13@gmail.com>
2022-10-08 16:29:47 +0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	file_heredoc.php

--- file_heredoc.php
+++ file_heredoc.php
@@ -1,12 +1,7 @@
 <?php
-<<<<<<< HEAD
 $tambahan = "programming";
 $kalimat = <<<habis
             Sedang Belajar Bahasa $tambahan Pemrograman PHP
-=======
-$kalimat = <<<habis
-            Sedang Belajar Bahasa Pemrograman PHP
->>>>>>> origin/master
             mohon untuk tidak di ganggu sedang serius!!! 
             habis;
 var_dump($kalimat);
