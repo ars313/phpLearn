fe73aae289a46ae5ec995a3a1f7de019ee7b95af
NURKHOLIS <kholis13@gmail.com>
2022-10-08 17:06:27 +0700

    adding value array without index

---
+++ Array/adding_array_no_index.php
@@ -0,0 +1,8 @@
+<?php
+$mix_array = array(121, "Dedi", 9.0, true, "Budi", "Heru");
+$mix_array[] = "Patrick";
+$mix_array[] = 89.99;
+$mix_array[] = "Gara";
+echo "<pre>";
+var_dump($mix_array);
+echo "</pre>";
