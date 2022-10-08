ea953e0138db36a8f1b40f13f7c84b6c8887022c
NURKHOLIS <kholis13@gmail.com>
2022-10-08 12:08:14 +0700

    Perbedaan quote di php

---
+++ single_double_quote.php
@@ -0,0 +1,8 @@
+<?php
+$kata = 'sepak';
+echo '$kata bola';
+echo "<br>";
+echo "$kata bola";
+echo "variable $situs ada www.duniailkom.com";
+echo "<br>";
+echo "variable \$situs ada www.duagniailkom.com";
