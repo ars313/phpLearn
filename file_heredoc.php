16b17e5b2e2abceba0f1ef35692a6f4a62d43d36
NURKHOLIS <kholis13@gmail.com>
2022-10-08 16:22:09 +0700

    heredoc php "<<<habis;"

---
+++ file_heredoc.php
@@ -0,0 +1,7 @@
+<?php
+$tambahan = "programming";
+$kalimat = <<<habis
+            Sedang Belajar Bahasa $tambahan Pemrograman PHP
+            mohon untuk tidak di ganggu sedang serius!!! 
+            habis;
+var_dump($kalimat);
