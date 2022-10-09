28723b07fd53e15ffd3e1109be4e74102d8bb9ed
NURKHOLIS <kholis13@gmail.com>
2022-10-09 22:31:14 +0700

    assign by array

---
+++ assigment_by_array.php
@@ -0,0 +1,14 @@
+<?php
+$siswa = [
+  0 => "Andri",
+  1 => "Joko",
+  2 => "Deden",
+  3 => "Sukma",
+  4 => "Ayu",
+  5 => "Febri"
+];
+
+$b = "Tahun 2000";
+$b .= " Dunia ilkom";
+echo $b;
+echo $siswa[3];
