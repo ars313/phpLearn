84826d1525590d9b56b8caa2ca70d44e231bfb57
Nurkholis <m_nurkholis@icloud.com>
2022-10-11 10:00:03 +0700

    Latihan mencari bilangna lebih besar atau lebih kecil

---
+++ FileCondition/lebih_kecil_besar.php
@@ -0,0 +1,15 @@
+<?php
+$a = 10;
+$b = 10;
+
+if ($a > $b){
+    echo "$a lebih besar dari $b";
+}
+
+elseif ($b > $a){
+    echo "bilangan $b lebih besar dari $a";
+}
+
+else {
+    echo "bilangan $a sama nilai dengan bilangan $b";
+}
