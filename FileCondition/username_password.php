4f56ae53c1f471454a8d3e69a05c579362356e56
Nurkholis <m_nurkholis@icloud.com>
2022-10-11 10:03:26 +0700

    latihan login sederhana

---
+++ FileCondition/username_password.php
@@ -0,0 +1,11 @@
+<?php
+$username= "Jono";
+$password = "asdf";
+
+if ($username == "Jono" and $password == "asdf"){
+    echo "Selamat anda bisa login";
+}
+
+else {
+    echo "Periksa kembali login anda";
+}
