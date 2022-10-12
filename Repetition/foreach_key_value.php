27c3cc9d962cc81cbbfce2216deec17825bc5183
Nurkholis <m_nurkholis@icloud.com>
2022-10-12 15:32:08 +0700

    foreach base on key and value

---
+++ Repetition/foreach_key_value.php
@@ -0,0 +1,12 @@
+<?php
+$nama = [
+    1 => "Dadang",
+    2 => "Herman",
+    3 => "Zarah",
+    4 => "Ina",
+    5 => "Farhat",
+];
+
+foreach ($nama as $key => $value) {
+    echo "Urutkan berdasarkan ke $key adalah $value <br>";
+}
