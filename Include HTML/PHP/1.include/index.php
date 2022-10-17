7930e99ebfebfe9f08cfd3c3bd90642b09a19f03
Nurkholis <m_nurkholis@icloud.com>
2022-10-17 07:39:27 +0700

    create index.php

---
+++ Include HTML/PHP/1.include/index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Belajar PHP</title>
+    <style>
+        /*Code CSS
+    </style>
+    <script>
+        //Code java Script
+    </script>
+</head>
+    <body>
+        <!--Header dari Website -->
+        <header>
+            <h1>Menu Navigasi</h1>
+            <h2>Logo</h2>
+        </header>
+        <article>
+            <p>Konten Halaman Home</p>
+        </article>
+        <!-- Footer dari website -->
+        <footer>
+            <p>Copyright 2019 Duniailkom</p>
+        </footer>
+    </body>
+</html>
