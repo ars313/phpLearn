ad90dc523c432c63749e0ac158e5c2c003ac76a5
Nurkholis <m_nurkholis@icloud.com>
2022-10-20 14:13:18 +0700

    index form processing proses.php

---
+++ Form Processing/1.menampilkan_hasil_form/proses.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Proses PHP</title>
+</head>
+    <body>
+        <h1>Halaman proses</h1>
+        <pre>
+            <?php
+                print_r($_GET)
+            ?>
+        </pre>
+    </body>
+</html>
