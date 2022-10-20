bc049ba9baffc6869f3b40d4abebf2e7db1b8ff2
Nurkholis <m_nurkholis@icloud.com>
2022-10-20 14:08:21 +0700

    index form processing method get

---
+++ Form Processing/menampilkan_hasil_form/index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Form PHP</title>
+</head>
+    <body>
+        <h1>Pemrosesan Form</h1>
+        <form action="proses.php" method="get">
+            <p>Nama : <input type="text" name="nama"></p>
+            <p>password : <input type="password" name="password"></p>
+            <input type="submit" value="Proses Data">
+        </form>
+    </body>
+</html>
