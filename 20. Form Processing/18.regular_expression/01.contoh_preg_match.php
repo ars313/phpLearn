44c4492f4677d552a56cea76f0f5868dca9c5e36
Nurkholis <m_nurkholis@icloud.com>
2022-10-31 14:16:38 +0700

    reguler expression pregmatch

---
+++ 20. Form Processing/18.regular_expression/01.contoh_preg_match.php
@@ -0,0 +1,8 @@
+<?php
+//  $hasil = preg_match("/[0-9]{5}/", "abcde");
+//  echo $hasil."<br>";    // 0
+
+  $hasil = preg_match("/^[0-9]{5}$/", "12345");
+  echo $hasil."<br>";    // 1
+  $hasil = preg_match("/[0-9]{5}$/", "23456");
+  echo $hasil."<br>";
