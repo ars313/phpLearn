9edc15ff4cbedc1399bc60142b622b3ae32d7028
Nurkholis <m_nurkholis@icloud.com>
2022-11-05 17:55:46 +0700

    ternary (variable = condition ? true : false;

--- test.php
+++ test.php
@@ -1,13 +1,19 @@
 <?php
-
-$_POST["kado"] = "a";
-
-if (isset($_POST["kado"])) {
-    $checked_pos = "checked";
-}
-
-else {
-    $checked_pos = "";
-}
-
-echo "$checked_pos";
+//
+//$_POST["kado"] = "a";
+//
+//if (isset($_POST["kado"])) {
+//    $checked_pos = "checked";
+//}
+//
+//else {
+//    $checked_pos = "";
+//}
+//
+//echo "$checked_pos";
+//$_POST["kado"] = "Buku Dunia Ilkom";
+//$checked_kado = isset($_POST["kado"]) ? $_POST["kado"] : "";
+//echo $checked_kado;
+$a = 9;
+$kondisi = $a > 5 ? "lebih besar dari 5" : "lebih kecil dari 5";
+echo $kondisi;
