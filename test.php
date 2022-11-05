e11cf94c44b0c5e070026d832efb71ef1a495e52
Nurkholis <m_nurkholis@icloud.com>
2022-11-05 17:55:46 +0700

    test php ternary condition

--- test.php
+++ test.php
@@ -1,13 +1,16 @@
 <?php
-
-$_POST["kado"] = "a";
-
-if (isset($_POST["kado"])) {
-    $checked_pos = "checked";
-}
-
-else {
-    $checked_pos = "";
-}
-
-echo "$checked_pos";
+//
+//$_POST["kado"] = "a";
+//
+//if (isset($_POST["kado"])) {
+//    $checked_pos = "checked";
+//}
+//
+//else {
+//    $checked_pos = "";
+//}
+//
+//echo "$checked_pos";
+$_POST["kado"] = "Buku Dunia Ilkom";
+$checked_kado = isset($_POST["kado"]) ? $_POST["kado"] : "";
+echo $checked_kado;
