558cf79596112da9f796f0aa8fd1aa2004280ee3
Nurkholis <m_nurkholis@icloud.com>
2022-11-06 09:43:02 +0700

    Coloascing operator

--- test.php
+++ test.php
@@ -14,6 +14,11 @@
 //$_POST["kado"] = "Buku Dunia Ilkom";
 //$checked_kado = isset($_POST["kado"]) ? $_POST["kado"] : "";
 //echo $checked_kado;
-$a = 9;
-$kondisi = $a > 5 ? "lebih besar dari 5" : "lebih kecil dari 5";
-echo $kondisi;
+//$a = 9;
+//$kondisi = $a > 5 ? "lebih besar dari 5" : "lebih kecil dari 5";
+//echo $kondisi;
+//
+//$_POST["kado"] = "Hello Kholis";
+$prize = "Hello prog";
+$kado = $_POST["kado"] ?? $prize ?? "";
+echo $kado;
